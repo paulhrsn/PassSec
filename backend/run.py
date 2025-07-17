6d050fd3fd735e4f882bcbdff5ee24904b499825
Paul Harrison <paulhar@umich.edu>
2025-07-16 22:00:23 -0500

    reduced run.py to just run the app

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,6 @@
 from app import create_app
-from db import db
 
-app = create_app() #creates flask app w/ config, routes, DB
+app = create_app()
 
-@app.before_first_request #creates tables if they dont exist yet
-def create_tables():
-    #auto-create tables from models before first request
-    db.create_all()
+if __name__ == "__main__":
+    app.run(debug=True)
