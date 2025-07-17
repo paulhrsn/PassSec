c0433f8e263459c081850765bde9c4d2ecc439d8
Paul Harrison <paulhar@umich.edu>
2025-07-16 22:10:58 -0500

    added register to auth_routes.py

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -2,22 +2,49 @@ from flask import Blueprint, request, jsonify
 from app.models.user import User
 from app.services.auth import check_password
 from flask_jwt_extended import create_access_token
+from app.services.auth import hash_password
+from db import db
 
 #set up flask blueprint for grouping auth routes
 auth_bp = Blueprint("auth", __name__, url_prefix="/api")
+
+
+#POST /api/register route
+
+auth_bp.route("/register", methods=["POST"])
+def register():
+    #get json from frontend, expecting {email, password}
+    data = request.get_json() or {}
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"error": "Email and password required"}), 400
+
+    #check if already exists
+    if User.query.filter_by(email=email).first():
+        return jsonify ({"message": "User already exists"}), 400
+    
+    hashed_pw = hash_password(password)
+    new_user = User(email=email, password=hashed_pw)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User created successfully"}), 201
+
 
 #POST /api/login route
 @auth_bp.route("/login", methods=["POST"])
 def login():
     #get json from frontend, expecting {email, password}
     data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
 
     #validate input fields exist
-    if not data or "email" not in data or "password" not in data:
+    if not email or not password:
         return jsonify({"error": "Email and password required"}), 400
-    
-    email = data["email"]
-    password = data["password"]
+
 
     #look for user in database
     user = User.query.filter_by(email=email).first()
@@ -34,4 +61,4 @@ def login():
     return jsonify({
         "token": token,
         "user": user.to_dict()
-    })
+    }), 200
