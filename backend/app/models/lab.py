5ab751323df1ba799aff935365a16533a93b7d8e
Paul Harrison <paulhar@umich.edu>
2025-07-18 13:12:37 -0500

    scaffolded lab model

---
+++ backend/app/models/lab.py
@@ -0,0 +1,20 @@
+# app/models/lab.py
+from app.extensions import db
+
+class LabScenario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    log_data = db.Column(db.Text, nullable=False)  #fake logs
+    question = db.Column(db.String, nullable=False)
+    choices = db.Column(db.PickleType, nullable=False)  #list of strings
+    answer = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "log_data": self.log_data,
+            "question": self.question,
+            "choices": self.choices,
+            "answer": self.answer,
+        }
